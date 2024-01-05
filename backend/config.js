8144a39aa398f3eaefd4c0a118970f680f2b0bb0
funkyd3v <95173888+funkyd3v@users.noreply.github.com>
2024-01-05 22:21:45 +0600

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://root:root@cluster1.f0x1v8g.mongodb.net/books-collection=true&w=majority';
+export const mongoDBURL = 'mongodb+srv://'test';
